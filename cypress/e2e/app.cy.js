bb51dc4aa071b162db1c81470643cc0bcd044b1c
Stephen Collins <sgcollins@outlook.com>
2023-04-20 17:34:15 -0400

    trying iframe selection

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,3 +1,31 @@
+const getIframeDocument = (...args) => {
+  return (
+    cy
+      .get("iframe[name*='__privateStripeFrame']", ...args)
+      // Cypress yields jQuery element, which has the real
+      // DOM element under property "0".
+      // From the real DOM iframe element we can get
+      // the "document" element, it is stored in "contentDocument" property
+      // Cypress "its" command can access deep properties using dot notation
+      // https://on.cypress.io/its
+      .its("0.contentDocument")
+      .should("exist")
+  );
+};
+
+const getIframeBody = (...args) => {
+  // get the document
+  return (
+    getIframeDocument(...args)
+      // automatically retries until body is loaded
+      .its("body")
+      .should("not.be.undefined")
+      // wraps "body" DOM element to allow
+      // chaining more Cypress commands, like ".find(...)"
+      .then({ timeout: 10000 }, cy.wrap)
+  );
+};
+
 describe("The Home Page", () => {
   it("successfully loads", () => {
     cy.visit("/");
@@ -7,6 +35,8 @@ describe("The Home Page", () => {
 
     cy.get(".App").should("exist");
 
-    cy.get("#Field-numberInput").type("4242424242424242");
+    getIframeBody()
+      .get("#Field-numberInput", { timeout: 10000 })
+      .type("4242424242424242");
   });
 });
